2255e193e74830d5fbefe41474eaa80dfb458ab2
salavisushant <87421852+salavisushant@users.noreply.github.com>
2021-11-18 17:51:02 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const user = require('./user.json');
+
+const app = express();  
+
+app.get('/users', (req, res) => {
+    return res.send(user)
+})
+
+app.get('/singleuser', (req, res) => {
+    return res.send("Welcome To Homepage")
+})
+
+
+app.listen(2000, function () {
+    console.log("listening on port 2000");
+});
+
